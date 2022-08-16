87227732c9ac4b215e7922401ba3c5a1dab59671
liuchang <itlc@qq.com>
2022-08-16 17:27:18 +0800

    example

--- Demo/Reflecter/ViewController.swift
+++ Demo/Reflecter/ViewController.swift
@@ -27,13 +27,72 @@ class ViewController: UIViewController {
         }
     }
 
+    // enum
+    enum Grade: AssociatedValueReflectable {
+        case one
+        case two
+        
+        var associatedValue: Any {
+            switch self {
+            case .one:
+                return 1
+            case .two:
+                return "two"
+            }
+        }
+    }
+    
+    class ClassPerson: DictionaryReflectable {
+        var name: String?
+        var age: Int?
+        var stature: Int?
+    }
+
+
+    class Student: ClassPerson {
+        var grade: Grade?
+    }
+
+    class School: DictionaryReflectable {
+        var students: [String: Student]?
+        var name: String?
+        var schoolmaster: Person?
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.white
-        let person = Person(name: "liuchang", age: 30, stature: 175, pId: 1)
-        let dict = person.transformToDictionary()
-        print(dict)
+//        let person = Person(name: "liuchang", age: 30, stature: 175, pId: 1)
+//        let dict = person.transformToDictionary()
+//        print(dict)
         // Do any additional setup after loading the view.
+        
+        let school = School()
+        school.name = "Peking University"
+        
+        let person = Student()
+        person.name = "jack"
+        person.age = 23
+        person.stature = 180
+        person.grade = .two
+
+        let person2 = Student()
+        person2.name = "zhang"
+        person2.age = 25
+        person2.stature = 182
+        person2.grade = .one
+        
+        school.students = ["jack":person, "zhang":person2]
+        
+        let person3 = ClassPerson()
+        person3.name = "liu"
+        person3.age = 50
+        
+        school.schoolmaster = Person(name: "liu", age: 50, stature: 175, pId: 1)
+        
+        let dict = school.transformToDictionary()
+
+        print(dict)
     }
 
 }
